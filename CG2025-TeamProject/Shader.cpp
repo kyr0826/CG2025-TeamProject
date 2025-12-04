f14d4704e265d341c0ac0e734b3aa3ca453e3c27
KYR(Victus) <kyr0826@tukorea.ac.kr>
2025-12-04 20:13:35 +0900

    Shader 유틸 함수, 쉐이더 링크 오류 검사 코드 추가

--- CG2025-TeamProject/Shader.cpp
+++ CG2025-TeamProject/Shader.cpp
@@ -3,13 +3,27 @@
 namespace Graphics {
 	Shader::Shader(std::string fragmentPath, std::string vertexPath) {
 		GLuint fragment, vertex;
-		ShaderUtils::make_fragmentShaders(fragmentPath.c_str(), fragment);
-		ShaderUtils::make_vertexShaders(vertexPath.c_str(), vertex);
+		if (!ShaderUtils::make_fragmentShaders(fragmentPath.c_str(), fragment)) {
+			std::cerr << "Failed to compile fragment shader: " << fragmentPath << std::endl;
+		}
+		if (!ShaderUtils::make_vertexShaders(vertexPath.c_str(), vertex)) {
+			std::cerr << "Failed to compile vertex shader: " << vertexPath << std::endl;
+		}
+		
 
 		shaderProgramID = glCreateProgram();
 		glAttachShader(shaderProgramID, vertex);
 		glAttachShader(shaderProgramID, fragment);
 		glLinkProgram(shaderProgramID);
+
+		GLint success = 0;
+		glGetProgramiv(shaderProgramID, GL_LINK_STATUS, &success);
+		if (!success) {
+			GLchar infoLog[512];
+			glGetProgramInfoLog(shaderProgramID, 512, NULL, infoLog);
+			std::cerr << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+		}
+
 		glDeleteShader(vertex);
 		glDeleteShader(fragment);
 	}
--- CG2025-TeamProject/Shader.h
+++ CG2025-TeamProject/Shader.h
@@ -7,6 +7,25 @@ namespace Graphics {
 
 		GLuint GetShaderProgramID() const { return shaderProgramID; }
 		void Activate() { glUseProgram(shaderProgramID); }
+
+		// 유틸 함수들
+		void SetMat4(const char* name, const glm::mat4& mat) const {
+			GLint loc = glGetUniformLocation(shaderProgramID, name);
+			if (loc != -1)
+				glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(mat));
+		}
+
+		void SetVec3(const char* name, const glm::vec3& vec) const {
+			GLint loc = glGetUniformLocation(shaderProgramID, name);
+			if (loc != -1)
+				glUniform3fv(loc, 1, glm::value_ptr(vec));
+		}
+
+		void SetInt(const char* name, int value) const {
+			GLint loc = glGetUniformLocation(shaderProgramID, name);
+			if (loc != -1)
+				glUniform1i(loc, value);
+		}
 	private:
 		GLuint shaderProgramID;
 	};
